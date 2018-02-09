b9fc522fedb0856d8f9b56393f33c478e003045b
James Orcutt <ej.orcutt@gmail.com>
2018-02-09 14:39:16 -0500

    Add PurchaseOrderParser as base for parsing PDFs. This will likely get pushed to a 2.0 feature, so won't make a separate branch for now

--- spec/purchase_order_parser_spec.rb
+++ spec/purchase_order_parser_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 describe PurchaseOrderParser do
   before(:all) do
-    @file_path = '/Users/eorcutt/Documents/Purchase Order Examples/LA COUNTY PO.pdf'
+    #@file_path = "#{Dir.pwd}/spec/test_files/LA\ COUNTY\ PO.pdf"
+    @file_path = "#{Dir.pwd}/spec/test_files/STATE\ OF\ CA\ PO.pdf"
     @parser = PurchaseOrderParser::Parser.new @file_path
-
     @la_po_number = 'PO-CC-18321923-1'
   end
 
   it 'should retrieve text' do
+    $stderr.puts "TEXT: #{@parser.text}"
     expect( @parser.text ).to_not be_empty
   end
 
@@ -17,10 +18,17 @@ describe PurchaseOrderParser do
   end
 
   it "should retrieve the purchase order number" do
+    $stderr.puts "NUMBER? #{@parser.number}"
     expect( @parser.number ).to eq @la_po_number
   end
 
+  it "should retrieve dates" do
+    $stderr.puts "DATEs? #{@parser.dates}"
+    expect( @parser.dates.length ).to be > 0
+  end
+
   it "should retrieve words/phrases from each line of mostly whitespace" do
+    $stderr.puts "GROUP INFO? #{@parser.lines_groups}"
     expect( @parser.lines.flatten.count ).to be > @parser.text.count
   end
 end
